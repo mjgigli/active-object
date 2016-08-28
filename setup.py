687b19e2fa333830d9e1bf8cdd9243b1089a2617
Matt Gigli <mgigli@trellisware.com>
2016-08-28 15:00:55 -0700

    Setup structure of active-object package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='active-object',
+      version='1.0',
+      description='Active Object Model Library',
+      author='Matt Gigli',
+      author_email='mjgigli@gmail.com',
+      packages=['ao'],
+     )
